976f39c68653f4e2a7e585d8af16ae0f444e3ed9
ebaumstarck <ebaumstarck@gmail.com>
2013-08-01 23:10:40 -0700

    moved cards into json. more jquery ui on the frontend

--- FlashCard/flashcards.py
+++ FlashCard/flashcards.py
@@ -1,9 +1,9 @@
 # encoding: utf-8
 import cgi
 import json
+import logging
 import os
 
-from google.appengine.ext import ndb
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -16,131 +16,57 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'])
 
-decks = [{
-  'name':'stack1',
-  'cards': [
-      { "english":"hello",
-      "chinese":"你好",
-      "pinyin":"ni3hao3"
-      },
-      { "english":"thanks",
-      "chinese":"谢谢",
-      "pinyin":"xie4xie4"
-      },
-      { "english":"welcome",
-      "chinese":"欢迎",
-      "pinyin":"huan1ying2"
-      },
-      { "english":"you are welcome",
-      "chinese":"不用谢",
-      "pinyin":"bu2yong4xie4"
-      },
-      { "english":"wait a moment",
-      "chinese":"等一下",
-      "pinyin":"deng3yi1xia4"
-      },
-      { "english":"drive a car",
-      "chinese":"开车",
-      "pinyin":"kai1che1"
-      },
-      { "english":"good",
-      "chinese":"好",
-      "pinyin":"hao3"
-      },
-       { "english":"bad",
-      "chinese":"坏",
-      "pinyin":"huai4"
-      },
-      { "english":"expensive",
-      "chinese":"贵",
-      "pinyin":"gui4"
-      },
-      { "english":"cheap",
-      "chinese":"便宜",
-      "pinyin":"pian2yi2"
-      },
-      { "english":"busy",
-      "chinese":"忙",
-      "pinyin":"mang2"
-      },
-      { "english":"difficult",
-      "chinese":"难",
-      "pinyin":"nan2"
-      },
-      { "english":"smart",
-      "chinese":"聪明",
-      "pinyin":"cong1ming2"
-      },
-      { "english":"pretty",
-      "chinese":"漂亮",
-      "pinyin":"piao4liang4"
-      },
-      { "english":"cute",
-      "chinese":"可爱",
-      "pinyin":"ke3ai4"
-      }
-  ]
-}, {
-  "name": "stack2",
-  'cards': [
-      { "english":"go",
-      "chinese":"去",
-      "pinyin":"qu4"
-      },
-      { "english":"cat",
-      "chinese":"猫",
-      "pinyin":"mao1"
-      }
-  ]
-}]
 
-class Card(ndb.Model):
-  cardId = ndb.IntegerProperty(required=True)
-  english = ndb.StringProperty(required=True)
-  pinyin = ndb.StringProperty(required=True)
-  chinese = ndb.StringProperty(required=True)
+def ReadDecks():
+  decks = []
+  json_dir = os.path.join(os.path.dirname(__file__), "json")
+  for dirname, dirnames, filenames in os.walk(json_dir):
+    for filename in filenames:
+      try:
+        decks.append(json.loads(open(
+            os.path.join(dirname, filename), "r").read()))
+      except Exception:
+        print "Fail loading flashcard deck %s" % filename
+  return decks
+
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    deckName = self.request.get("deckName")
+  def get(self, *args):
+    decks = ReadDecks()
+    deck_name = None
+    if args:
+      deck_name = args[0]
 
-    user = users.get_current_user()
-    login_url = None
-    logout_url = None
-    if user:
-      logout_url = users.create_logout_url('/')
-    else:
-      login_url = users.create_login_url(self.request.uri)  # 'http://www.google.com'
+    # user = users.get_current_user()
+    # login_url = None
+    # logout_url = None
+    # if user:
+    #   logout_url = users.create_logout_url('/')
+    # else:
+    #   login_url = users.create_login_url(self.request.uri)  # 'http://www.google.com'
 
     path = os.path.join(os.path.dirname(__file__), 'FlashCardChEng.html')
 
     # Find which deck they want.
     deck = None
-    if deckName:
-      validDecks = [d for d in decks if d["name"] == deckName]
-      if validDecks:
-        deck = validDecks[0]
-    elif decks:
+    if deck_name:
+      valid_decks = [d for d in decks if d["name"] == deck_name]
+      if valid_decks:
+        deck = valid_decks[0]
+    if not deck:
       deck = decks[0]
 
-    deckNames = [t["name"] for t in decks]
     template_args = {
-        #'content': cards,
-        'content_json': json.dumps(deck["cards"]),
-        # 'userGreeting':userGreeting,
-        'user': user,
-        'login_url': login_url,
-        'logout_url': logout_url,
-        'deckNames': deckNames,
-        #'variable': 42,
+        "deck_found": bool(deck),
+        "content_json": json.dumps(deck["cards"]) if deck else "",
+        # 'user': user,
+        # 'login_url': login_url,
+        # 'logout_url': logout_url,
+        "deck_names": [d["name"] for d in decks],
     }
     self.response.out.write(template.render(path, template_args))
 
-# ？class DeckHandler(webapp2.RequestHandler):
-#   def get(self, deckName):
-#     self.response.write("So you want deck '%s', eh?" % deckName)
-
 application = webapp2.WSGIApplication([
-    ('/', MainPage),
-    # (r'/deck/(.*)', DeckHandler),
+    ("/", MainPage),
+    ("/(.*)", MainPage),
 ])
--- FlashCard/js/main.js
+++ FlashCard/js/main.js
@@ -1,115 +1,78 @@
-﻿$(document).ready(function(){
-  var content = $("#flashcard").data("json"),
-	 i = 0,// the index of the entry in the word list
-    answer = {},// the chinese,pinyin entry saved for display after user click answer button 
-    wrongWords  = []//list for saving wrong words to review later
-    ;
-// create menu for all the decks of cards
-  $('#menu').menu({
-    select: function(event,ui){
+﻿
+// the flashcards in the deck
+var flashcards = [];
+// which flashcard we are showing
+var flashcardi = -1;
+
+// initializes the deck of flashcards
+function initDeck() {
+  flashcards = $("#flashcard").data("json");
+  flashcardi = flashcards.length - 1;
+}
+
+// shuffles the deck of flashcards
+function shuffle() {
+  // fisher-yates shuffle algorithm
+  var count = flashcards.length;
+  if (count > 0) {
+    while (--count){
+      var newCount = Math.floor(Math.random() * (count + 1)),
+        temp = flashcards[count],
+        temp1 = flashcards[newCount];
+      flashcards[count] = temp1;
+      flashcards[newCount] = temp;
+    }
+  }
+}
+
+// display the current flashcard and increment the counter
+function displayNext(list) {
+  // reset the flash card border color
+  // $("#flashcard").css("border-color","rgb(53,155,198)");
+  if (flashcardi < flashcards.length){
+    var card = flashcards[flashcardi];
+    $("#english").html(card.english);
+    $("#chinese").html(card.chinese);
+    $("#pinyin").html(card.pinyin);
+    flashcardi++;
+  } 
+  //display the first word
+  if (flashcardi == flashcards.length){
+    flashcardi = 0;
+  }
+}
+
+//internal variable for save the javascript function
+var auto = null;
+// start or stop auto show a new flashcard every 2 seconds
+function autoDisplay() {
+  if (auto) {
+    window.clearInterval(auto);
+    auto = null;
+  } else {
+    auto = setInterval(displayNext, 2000);
+  }
+}
+
+$(document).ready(function() {
+  initDeck();
+  // create menu for all the decks of cards
+  $("#menu").menu({
+    select: function(event, ui) {
       $.ajax({
         url: "/deck/" + ui.item.text()
-        // context: document.body
-      })
-      .done(function(data) {
       });
     }
   });
-  // display the word by clicking the next button
-  function display(list){
-  // reset the flash card border color
-    $('#flashcard').css('border-color','rgb(53,155,198)');
-    if( i <  list.length){
-      // clear the chinese and pinyin 
-      $('#chinese').empty();
-      $('#pinyin').empty();
-      // display the english flash card
-      $('#english').html(list[i].english);
-      //save the chinese and pinyin answer for later
-      answer.chinese = list[i].chinese;
-      answer.pinyin = list[i].pinyin;
-      
-      i++;
-    } 
-    //display the first word
-    if( i == list.length){
-      i = 0;
-    }
-  }
-  // shuffle the flash card
-  function shuffle(){
-    // fisher-yates shuffle algorithm
-    var 
-      count = content.length;
-    if( count > 0){
-      while( --count){
-      var newCount = Math.floor(Math.random() * (count + 1)),
-        temp = content[count],
-        temp1 = content[newCount];
-      content[count] = temp1;
-      content[newCount] = temp;
-      }
-    }
-  }
-  //internal variable for save the javascript function
-  var auto;
-  //auto display the flash card per 1 second
-  function autoDisplay(){
-    var interval = 2000;//2000 millionseconds 
-    auto = setInterval(function(){
-      if( i <  content.length){
-        // display the english flash card
-        $('#english').html(content[i].english);
-        $('#chinese').html(content[i].chinese);
-        $('#pinyin').html(content[i].pinyin);
-        i++;
-      } 
-      //display the first word
-      if( i == content.length){
-        i = 0;
-      }
-    },2000);
-  }
-  // stop auto display
-  function stopAutoDisplay(){
-    window.clearInterval(auto);
-  }
 
-  //click the button 
-  $('#next')
-    .button()
-    .click(function() {
-      display(content);
-    });
-  $('#shuffle')
-    .button()
-    .click(shuffle);
-  $('#auto')
-    .button()
-    .click(autoDisplay);
-  $('#stop')
-    .button()
-    .click(stopAutoDisplay);
-  // display the answer when the user click the show the answer button
-  $('#answer')
-    .button()
-    .click(function(){
-      $('#chinese').html(answer.chinese);
-      $("#pinyin").html(answer.pinyin);
-    });
-  $('#reviewBtn')
-    .button()
-    .click(function(){
-      var wrongWord = content[i-1];
-      wrongWords.push(wrongWord);
-      $('#flashcard').css('border-color','red');
-      //if($('#next').click()){
-      //	$('#flashcard').css('border-color','4d90fe');
-      //}
-    });
-  $('#reviewList')
-    .button()
-    .click(function(){
-  
-    });
+  $("#auto").button().click(autoDisplay);
+  $("#next").button().click(displayNext);
+  $("#shuffle").button().click(shuffle);
+  $("#show").buttonset();
+  $("#show input").click(function() {
+    var id = $(this).attr("id");
+    $(".front > div").appendTo($(".back"));
+    var languageId = id.match(/^show(.*)$/)[1].toLowerCase();
+    $("#" + languageId).appendTo($(".front"));
   });
+});
